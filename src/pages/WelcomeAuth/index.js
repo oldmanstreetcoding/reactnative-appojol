709790de60d1c8b76bb39ab53dd7e840c0af15cc
newstreetcoding <furqan.right.here@gmail.com>
2020-05-07 01:21:23 +0700

    membuat project baru dari kerangka project react native

---
+++ src/pages/WelcomeAuth/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const WelcomeAuth = () => {
+    return (
+        <View>
+            <Text>Welcome Auth Page</Text>
+        </View>
+    );
+};
+
+export default WelcomeAuth;
+
